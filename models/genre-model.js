1dcc5c5bfea2c4852174a3c1f5482c11244ea645
Gabemb <gabrielbny@gmail.com>
2016-12-01 13:00:23 -0500

    add song & genre model schemes

--- models/genre-model.js
+++ models/genre-model.js
@@ -5,5 +5,15 @@ const sequelizeConnection = require('../db');
 // YOUR CODE HERE:
 //////////
 
+const Genre = sequelizeConnection.define('genre', {
+	title: {
+		type: Sequelize.STRING,
+		validate: {
+			max: 100,
+			notNull: true,
+			notEmpty: true
+		}
+	}
+})
 
 module.exports = Genre;
--- models/song-model.js
+++ models/song-model.js
@@ -1,9 +1,33 @@
 const Sequelize = require('sequelize');
 const sequelizeConnection = require('../db');
 
+//models
+const Artist = require('./artist-model');
+
 //////////
 // YOUR CODE HERE:
 //////////
 
+const Song = sequelizeConnection.define('song', {
+	title: {
+		type: Sequelize.STRING,
+		validate: {
+			max: 250,
+			notNull: true,
+			notEmpty: true
+		}
+	},
+	youtube_url: {
+		type: Sequelize.STRING,
+		validate: {
+			max: 50,
+			notNull: true,
+			notEmpty: true,
+			isUrl: true 
+		}
+	},
+})
+
+Song.belongsTo(Artist);
 
 module.exports = Song;
